2d76e673ffdaf7e50970aeaccfe65380fb8733d7
arfanamd <arfanachmad11@gmail.com>
2023-06-15 11:37:17 +0700

    ch: code improvement
    mod: zscrap-daily.sh
    
    add feature to specify the date

--- zscrap-daily.sh
+++ zscrap-daily.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
-# usage: zscrap-daily.sh
+# usage: zscrap-daily.sh [date]
+# date format is mm/dd/yy
 set -euf -o pipefail
 
 # check user
@@ -11,13 +12,15 @@ fi
 # valid email admin account
 _SCRAP_ACC='admin@'
 _ZMSEARCH='/opt/zimbra/bin/zmmailbox'
+
+# Zimbra by default generates the daily report on the midnight.
 # using the date before today
-_DATE=$(date --date='day ago' +'%x')
+_DATE="${1:-$(date --date='day ago' +'%x')}"
 _KEYSEARCH="in:inbox date:${_DATE} Daily"
 
 # get msg id
 _ZMSGID=$(${_ZMSEARCH} -z -m ${_SCRAP_ACC} s -l 1 "${_KEYSEARCH}" | awk '/Daily/ {print $2}') && _ZMSGID=${_ZMSGID/-/}
 
 # get delivered result
-printf -- "%s: " "$(date --date='day ago' +'%c')"
-${_ZMSEARCH} -z -m ${_SCRAP_ACC} gm ${_ZMSGID} | grep -E '[0-9]+...delivered'
+_ZMSG="$(${_ZMSEARCH} -z -m ${_SCRAP_ACC} gm ${_ZMSGID} | grep -E '[0-9]+...delivered')"
+printf -- "%s: %s\n" "$(date --date=${_DATE} +'%a, %d %b %y')" "${_ZMSG:='NOTHING'}"
